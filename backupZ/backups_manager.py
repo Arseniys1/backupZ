88e3232309803146c0c9ba60ae960eaad3373637
Arseniy <thevalakas1@gmail.com>
2025-01-21 18:15:20 +0500

    проверка важных директив

--- backupZ/backups_manager.py
+++ backupZ/backups_manager.py
@@ -1,3 +1,4 @@
+from backupZ.check_directives import CheckMainBlockDirectives
 
 
 class BackupsManager:
@@ -10,10 +11,15 @@ class BackupsManager:
     def start_watching_time(self):
         self._walk_trough_blocks()
 
+    def _check_important_directives(self, block):
+        main_block_checker = CheckMainBlockDirectives(block)
+        return main_block_checker.check_directives()
+
     def _walk_trough_blocks(self):
         for main_block in self._config.blocks:
-            for directive in main_block.directives:
-                pass
-
-
-
+            if not self._check_important_directives(main_block):
+                print(
+                    f"Для блока {main_block.name} в файле: {main_block.source_file} строка: {main_block.line_num}. Не указаны или не верно указаны важные директивы. Пропускаю его.")
+                continue
+            # for directive in main_block.directives:
+            #     pass
---
+++ backupZ/check_directives.py
@@ -0,0 +1,39 @@
+import os
+from datetime import datetime
+
+
+class CheckMainBlockDirectives:
+    def __init__(self, block):
+        self._block = block
+        self._dir_directive_found = False
+        self._time_directive_found = False
+
+    def check_directives(self):
+        for directive in self._block.directives:
+            if directive.key == "Dir":
+                if self._validate_dir_directive(directive):
+                    self._dir_directive_found = True
+            elif directive.key == "Time":
+                if self._validate_time_directive(directive):
+                    self._time_directive_found = True
+
+        if self._dir_directive_found and self._time_directive_found:
+            return True
+
+    def _validate_dir_directive(self, directive):
+        if os.path.isdir(directive.value):
+            return True
+        else:
+            print(f"Не верно указан путь у директивы Dir. В файле: {directive.source_file} строка: {directive.line_num}")
+
+    def _validate_time_directive(self, directive):
+        try:
+            # Пытаемся преобразовать строку в объект datetime
+            datetime.strptime(directive.value, '%H:%M')
+            return True
+        except ValueError:
+            # Если возникает ошибка, значит строка не соответствует формату
+            return False
+
+
+
